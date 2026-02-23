dbd848e20d847756c67f6950cdda5d877e60764e
Fabiano <fabianoquirinos@outlook.com>
2026-02-23 14:57:18 -0300

    UserService

--- dscommerce/src/main/java/com/devsuperior/projeto1/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/devsuperior/projeto1/dscommerce/entities/User.java
@@ -124,6 +124,19 @@ public class User implements UserDetails {
         return orders;
     }
 
+    public void addRole(Role role){
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName){
+        for(Role role : roles){
+            if(role.getAuthority().equals(roleName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
